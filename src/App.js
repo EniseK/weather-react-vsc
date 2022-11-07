f9e161e1acfee2f639d5714f6851dc435e1ae218
EniseK <enisekizilaslan@gmail.com>
2022-11-07 12:03:11 +0100

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React from "react";
 import WeatherSearch from "./WeatherSearch";
 function App() {
 	return (
-		<div className="App">
+		<div className="Weather App">
 			<header className="App-header">
 				<h1>Weather Search Engine</h1>
 				<WeatherSearch />
 				<br />
-				<a href="https://github.com/EniseK/weather-react-vsc">
-					<small>Project link to GitHub</small>
-				</a>
+				<footer>
+					This project was coded by Enise Kizilaslan and is {""}
+					<a
+						href="https://github.com/EniseK/weather-react-vsc"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open-sourced on GitHub
+					</a>
+				</footer>
 			</header>
 		</div>
 	);
